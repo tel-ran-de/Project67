cb1840ae99baee5093b1910ff9ffdbbf441fa87b
o.podlubnaja <o.podlubnaja@hirschtec.eu>
2020-01-09 19:22:02 +0100

    Add user dto

---
+++ src/main/java/de/telran/blog/dto/UserDto.java
@@ -0,0 +1,27 @@
+package de.telran.blog.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class UserDto {
+    @NotNull
+    @NotEmpty
+    private String firstName;
+
+    @NotNull
+    @NotEmpty
+    private String lastName;
+
+    @NotNull
+    @NotEmpty
+    private String password;
+    private String matchingPassword;
+
+    @NotNull
+    @NotEmpty
+    private String email;
+
+}
