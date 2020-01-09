a7a740d8f41af8a4ba7a9497f317d53fb868fa7e
o.podlubnaja <o.podlubnaja@hirschtec.eu>
2020-01-09 19:39:41 +0100

    Update mapping

--- src/main/java/de/telran/blog/controller/RegistrationController.java
+++ src/main/java/de/telran/blog/controller/RegistrationController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 
 @RestController
-@RequestMapping("/api/post")
+@RequestMapping("/user/registration")
 public class RegistrationController {
 
-    @RequestMapping(value = "/user/registration", method = RequestMethod.GET)
+    @GetMapping
     public String showRegistrationForm(WebRequest request, Model model) {
         UserDto userDto = new UserDto();
         model.addAttribute("user", userDto);
