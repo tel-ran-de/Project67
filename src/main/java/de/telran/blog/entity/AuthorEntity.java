3805005d9585d87b7679e918fa49fc4b6c9d2f25
JanKelsch <kelsh@bigmir.net>
2020-01-14 17:36:40 +0100

    Added default author with id=1 when app starts

--- src/main/java/de/telran/blog/entity/AuthorEntity.java
+++ src/main/java/de/telran/blog/entity/AuthorEntity.java
@@ -1,16 +1,25 @@
 package de.telran.blog.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Data
 public class AuthorEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(
+            strategy= GenerationType.AUTO,
+            generator="native"
+    )
+    @GenericGenerator(
+            name = "native",
+            strategy = "native"
+    )
     private Long id;
     private String firstName;
     private String lastName;
