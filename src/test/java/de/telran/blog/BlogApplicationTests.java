085b329857780f3f1e69ac8a0667467d894c1a16
Alex Loginov <alexander.loginov@takeaway.com>
2020-01-23 20:54:07 +0100

    Fixing tests

--- src/test/java/de/telran/blog/BlogApplicationTests.java
+++ src/test/java/de/telran/blog/BlogApplicationTests.java
@@ -2,12 +2,12 @@ package de.telran.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class BlogApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
